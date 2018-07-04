d133bafa93418c89939c8902578ad465f8a8ca95
xujing <jink_xu@foxmail.com>
2018-07-04 17:54:46 +0800

    Add Class member & Accessibility

--- src/clang_rs_ext.rs
+++ src/clang_rs_ext.rs
@@ -1,13 +1,31 @@
 extern crate clang;
 
 use clang::*;
-use std::env;
+// use std::env;
+use std::fmt;
+use std::fmt::Debug;
 
 
 pub trait EntityExt {
     fn get_file_path(&self) -> Option<String>;
     fn get_file_line(&self) -> Option<u32>;
     fn print_info(&self);
+}
+
+pub trait DisplayBridge {
+    fn fmt_bridge(&self, f: &mut fmt::Formatter) -> fmt::Result;
+}
+
+impl fmt::Display for DisplayBridge {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.fmt_bridge(f)
+    }
+}
+
+impl DisplayBridge for clang::Accessibility {
+    fn fmt_bridge(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Debug::fmt(self, f)
+    }
 }
 
 impl<'tu> EntityExt for Entity<'tu> {
@@ -23,9 +41,27 @@ impl<'tu> EntityExt for Entity<'tu> {
             .map(|location| location.line)
     }
     fn print_info(&self) {
+        let print_info_general = || {
+            println!("{} : {:?}, type: {}, Location: {}:{}"
+                     , self.get_name().unwrap_or(String::from("!!!Unknow!!!"))
+                     , self.get_kind()
+                     , self.get_type()
+                         .map(|e| e.get_display_name())
+                         .unwrap_or(String::from("!!!Unknow!!!"))
+                     , self.get_file_path()
+                         .unwrap_or(String::from("!!!Unprintable!!!"))
+                     , self.get_file_line()
+                         .map(|u| u as i64)
+                         .unwrap_or(-1)
+            )
+        };
         match self.get_kind() {
+            EntityKind::ClassDecl | EntityKind::ClassTemplate | EntityKind::ClassTemplatePartialSpecialization => {
+                print_info_general();
+                self.get_children().iter().for_each(|e| e.print_info());
+            },
             EntityKind::Method => {
-                println!("{}::{} : {:?}, type: {}, Location: {}:{}"
+                println!("{}::{} : {:?}, type: {}, Accessibility: {}, Location: {}:{}"
                          , self.get_semantic_parent()
                              .and_then(|e| e.get_name())
                              .unwrap_or(String::from(""))
@@ -33,6 +69,9 @@ impl<'tu> EntityExt for Entity<'tu> {
                          , self.get_kind()
                          , self.get_type()
                              .map(|e| e.get_display_name())
+                             .unwrap_or(String::from("!!!Unknow!!!"))
+                         , self.get_accessibility()
+                             .map(|e| (&e as &DisplayBridge).to_string())
                              .unwrap_or(String::from("!!!Unknow!!!"))
                          , self.get_file_path()
                              .unwrap_or(String::from("!!!Unprintable!!!"))
@@ -42,18 +81,7 @@ impl<'tu> EntityExt for Entity<'tu> {
                 );
             },
             _ => {
-                println!("{} : {:?}, type: {}, Location: {}:{}"
-                         , self.get_name().unwrap_or(String::from("!!!Unknow!!!"))
-                         , self.get_kind()
-                         , self.get_type()
-                             .map(|e| e.get_display_name())
-                             .unwrap_or(String::from("!!!Unknow!!!"))
-                         , self.get_file_path()
-                             .unwrap_or(String::from("!!!Unprintable!!!"))
-                         , self.get_file_line()
-                             .map(|u| u as i64)
-                             .unwrap_or(-1)
-                );
+                print_info_general();
             }
         }
     }
